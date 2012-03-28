d02ffe6288b9fdb2584e66f2509e7a69da3b2afd
Mark Simpson <verdammelt@gmail.com>
2012-03-27 23:23:13 -0400

    slowly adding persistance - now have a 'form' which will allow entry of drink names

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject defdrink "0.0.1-SNAPSHOT"
             :description "Web application to suggest drinks based upon the contents of your liquor cabinet."
             :dependencies [[org.clojure/clojure "1.3.0"]
+                           [postgresql/postgresql "9.1-901.jdbc4"]
+                           [org.clojure/java.jdbc "0.1.3"]
                            [noir "1.2.1"]]
             :main defdrink.server)
-
--- src/defdrink/views/welcome.clj
+++ src/defdrink/views/welcome.clj
@@ -1,15 +1,30 @@
 (ns defdrink.views.welcome
-  (:require [defdrink.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
+  (:require [defdrink.views.common :as common])
+  (:use [noir.core :as noir]
+        [hiccup.core :as html]
+        [hiccup.form-helpers :as form]))
 
-(defpage "/welcome" []
+(noir/defpartial welcome-text []
+            [:div
+              [:p "(defdrink) will help you choose drinks based upon what you have in your liquor cabinet."]
+
+              [:p "It will let you define what you have in your cabinet, to rate cocktails (so that ones you don't like one be suggested and ones you love will be suggested first"]
+
+              [:p "Let's start simply. You can add the names of drinks and (defdrink) will remember them"]])
+
+(noir/defpartial new-drink-form []
+                 (label "name" "Drink Name:")
+                 (text-field "name" "")
+                 (form/submit-button "(defdrink)")
+                 )
+
+
+(noir/defpage "/welcome" []
          (common/layout
+            (welcome-text) 
+            (form/form-to [:post "/welcome"]
+                          (new-drink-form))
+            ))
 
-           [:p "(defdrink) will help you choose drinks based upon what you have
-               in your liquor cabinet."]
-
-           [:p "It will let you define what you have in your cabinet, to rate
-               cocktails (so that ones you don't like one be suggested and ones
-                             you love will be suggested first"]))
+(noir/defpage [:post "/welcome"] []
+              (render "/welcome"))
