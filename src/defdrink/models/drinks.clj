20fdc1dbb7b993f3c71cf8991c6f39e13a9900c0
Mark Simpson <verdammelt@gmail.com>
2012-03-31 15:11:39 -0400

    adding persistence of drink names

--- src/defdrink/models/drinks.clj
+++ src/defdrink/models/drinks.clj
@@ -2,6 +2,12 @@
   (:require [clojure.java.jdbc :as sql]))
 
 (defn all []
-  [])
+  (sql/with-connection (System/getenv "DATABASE_URL")
+                       (sql/with-query-results results
+                                               ["select * from drinks order by name asc"]
+                                               (into [] results))))
 
-(defn insert [name])
+(defn insert [name]
+  (sql/with-connection (System/getenv "DATABASE_URL")
+                       (sql/insert-record :drinks {:name name})))
+
