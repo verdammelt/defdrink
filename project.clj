cd4fad91dbcb7803f49f234474bbb3d3ad3d1295
Mark Simpson <verdammelt@gmail.com>
2012-04-23 20:59:23 -0400

    Got Midje (with Lazytest feature) working - still no tests

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
                            [postgresql/postgresql "9.1-901.jdbc4"]
                            [org.clojure/java.jdbc "0.1.3"]
                            [noir "1.2.1"]]
+            :dev-dependencies [[lein-midje "1.0.9"]
+                               [com.stuartsierra/lazytest "1.2.3"]]
+            :repositories {"stuart" "http://stuartsierra.com/maven2"}
             :main defdrink.server)
--- src/defdrink/models/drink.clj
+++
@@ -1,3 +0,0 @@
-(ns defdrink.models.shout
-  (:require [clojure.java.jdbc :as sql]))
-
