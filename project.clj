0c3840cea7d4f5e1d0258a8cd6b087f949e0c6c2
Mark Simpson <verdammelt@gmail.com>
2012-04-23 21:30:08 -0400

    Added single fake midje test - and finished the midje setup so it passes.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
                            [postgresql/postgresql "9.1-901.jdbc4"]
                            [org.clojure/java.jdbc "0.1.3"]
                            [noir "1.2.1"]]
-            :dev-dependencies [[lein-midje "1.0.9"]
+            :dev-dependencies [[midje "1.3.0"]
+                               [lein-midje "1.0.9"]
                                [com.stuartsierra/lazytest "1.2.3"]]
             :repositories {"stuart" "http://stuartsierra.com/maven2"}
             :main defdrink.server)
---
+++ test/defdrink/test/models/drinks.clj
@@ -0,0 +1,5 @@
+(ns defdrink.test.models.drinks
+  (:use clojure.test)
+  (:use midje.sweet))
+
+(fact (+ 1 1) => even?)
