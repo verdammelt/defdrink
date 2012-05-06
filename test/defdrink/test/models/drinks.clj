d399f7a9fce1d525516706443134e900c4a0c5d4
Mark Simpson <verdammelt@gmail.com>
2012-05-05 20:02:27 -0400

    Tweaked the test for inserting - i think it reads better now - still unsure if it is really good

--- test/defdrink/test/models/drinks.clj
+++ test/defdrink/test/models/drinks.clj
@@ -4,8 +4,10 @@
   (:use defdrink.models.drinks)
   (:require [defdrink.utils.sql :as sql]))
 
-(fact (all) => [...drink1... ...drink2...]
-      (provided (sql/select :drinks) => [...drink1... ...drink2...])
+(fact "getting drinks from database"
+      (all) => [...drink1... ...drink2...]
+      (provided (sql/select :drinks) => [...drink1... ...drink2...]))
 
-      (insert ...name...) => ...not-important...
-      (provided (sql/insert :drinks {:name ...name...}) => ...not-important...))
+(fact "inserting new drinks into the database"
+      (insert ...name...) => irrelevant
+      (provided (sql/insert :drinks {:name ...name...}) => irrelevant))
