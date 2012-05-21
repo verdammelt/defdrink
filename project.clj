a9f9e23b2765b6343a18239628448a7110e7008d
Mark Simpson <verdammelt@gmail.com>
2012-05-20 20:50:26 -0400

    Updating all versions of libraries

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject defdrink "0.0.1-SNAPSHOT"
-            :description "Web application to suggest drinks based upon the contents of your liquor cabinet."
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [postgresql/postgresql "9.1-901.jdbc4"]
-                           [org.clojure/java.jdbc "0.1.3"]
-                           [noir "1.2.1"]]
-            :dev-dependencies [[midje "1.4.0-beta2"]
-                               [com.stuartsierra/lazytest "1.2.3"]]
-            :repositories {"stuart" "http://stuartsierra.com/maven2"}
-            :main defdrink.server)
+  :description "Web application to suggest drinks based upon the
+  contents of your liquor cabinet."
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [org.clojure/java.jdbc "0.2.1"]
+                 [noir "1.2.1"]]
+  :dev-dependencies [[midje "1.4.0"]
+                     [com.stuartsierra/lazytest "1.2.3"]]
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}
+  :main defdrink.server)
